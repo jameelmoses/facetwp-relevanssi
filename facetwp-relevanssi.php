b532d3823e41313293a65b091f33d7358bae5baf
mgibbs189 <mgibbs189@gmail.com>
2016-06-15 16:22:16 -0400

    Tagged 0.5

--- facetwp-relevanssi.php
+++ facetwp-relevanssi.php
@@ -3,29 +3,14 @@
 Plugin Name: FacetWP - Relevanssi integration
 Plugin URI: https://facetwp.com/
 Description: Relevanssi integration for FacetWP
-Version: 0.4
-Author: Matt Gibbs
-GitHub Plugin URI: https://github.com/FacetWP/facetwp-relevanssi
-
-Copyright 2015 Matt Gibbs
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+Version: 0.5
+Author: FacetWP, LLC
+GitHub URI: facetwp/facetwp-relevanssi
 */
 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) or exit;
 
+include( dirname( __FILE__ ) . '/github-updater.php' );
 
 class FacetWP_Relevanssi
 {
